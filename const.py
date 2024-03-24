3331faaa65a34ae24a3c5082330e4cdfda5adca8
JeffreyGaydos <jeff13gaydos@gmail.com>
2024-03-24 01:42:18 -0400

    add discord embed links

--- const.py
+++ const.py
@@ -8,3 +8,8 @@ _newIndicator = "NEW: "
 _bandcampCollectionAPI = "https://bandcamp.com/api/fancollection/1"
 _releasesFolder = "Releases"
 _musicPostfix = "/music"
+_bandcampFridayGifs = [
+    "https://media.discordapp.net/attachments/971520732544766002/1221323444453376112/BandcampFriday1.gif?ex=66122918&is=65ffb418&hm=80ded9ee523c51ace839fe231cf5079294e9badeca9f714046016cd139d93588&=",
+    "https://media.discordapp.net/attachments/971520732544766002/1221323444810154004/BandcampFriday2.gif?ex=66122918&is=65ffb418&hm=0c345350097384a5678287b85123659c294aff4baa9b4bc3bd76f8eee09e6a39&=",
+    "https://media.discordapp.net/attachments/971520732544766002/1221323445217005668/BandcampFriday3.gif?ex=66122918&is=65ffb418&hm=06b0142b915e51f0e11ad205f99ae2428cdeb5c7631064cd7a0a0359334036a5&="
+]
